d4bf5b43b3b4aa82dc4506e529a52e3ea65dce57
Remmington Damper <Kincaiddamper@gmail.com>
2021-11-04 14:26:01 -0400

    Finished first 2 sections

--- Example/Tests/FlexDataSourceItemTests.swift
+++ Example/Tests/FlexDataSourceItemTests.swift
@@ -29,6 +29,7 @@ class FlexDataSourceItemTests: XCTestCase {
     }
     // go to FlexDataSourceItems and take a look. We want test all configureCell functions to make sure call what we pass into the constructors ex:
     
+    
     func testFunctionalFlexDataSourceItem() {
         let identifier = "TableViewCell"
         let item = FunctionalFlexDataSourceItem<UITableViewCell>(identifier: identifier, set(\UITableViewCell.backgroundColor, .red))
@@ -49,12 +50,34 @@ class FlexDataSourceItemTests: XCTestCase {
     }
     
     func testTappableFlexDataSourceItem() {
-        var wasTapped = false
+        var pressed = false
+        let cellID = "Tappable"
+        let cell = UITableViewCell()
+        let item = TappableFunctionalFlexItem<UITableViewCell>(identifier: cellID, set(\UITableViewCell.backgroundColor, .red)) {
+            pressed = true
+        }
+        item.configureCell(cell)
+        item.onTap()
+        XCTAssertEqual(cell.backgroundColor, .red)
+        XCTAssertEqual(item.cellIdentifier(), cellID)
+        XCTAssertTrue(pressed == true)
         // pass a closure to change this value in the constructor, make sure is true after calling onTap
     }
     
     func testSwipableFlexDataSourceItem() {
         var wasSwiped = false
+        var wasTapped = false
+        let cellID = "Swipable"
+        let cell = UITableViewCell()
+        let item = SwipableItem<UITableViewCell>(identifier: cellID, set(\UITableViewCell.backgroundColor, .blue), { wasTapped = true }, {wasSwiped = true})
+        item.configureCell(cell)
+        item.onTap()
+        item.onSwipe()
+        XCTAssertEqual(cell.backgroundColor, .blue)
+        XCTAssertEqual(item.cellIdentifier(), cellID)
+        XCTAssertTrue(wasSwiped == true)
+        XCTAssertTrue(wasTapped == true)
+        
         // repeat for onSwipe
     }
 }
--- Example/Tests/FlexDataSourceSectionTests.swift
+++ Example/Tests/FlexDataSourceSectionTests.swift
@@ -7,22 +7,70 @@
 //
 
 import XCTest
+import FlexDataSource
 
 class FlexDataSourceSectionTests: XCTestCase {
     // look at FlexDataSourceSection and test the methods therein
     func testTitle() {
+        let title = "Section Testing"
+        let flexSection = FlexDataSourceSection(title: title, items: nil)
         // make sure the title is the one passed into the constructor
+        XCTAssertEqual(flexSection.title!, title)
     }
     
     func testItems() {
         // make sure the items are properly transferred into the section
+        let title = "Section Testing"
+        let type = ConcreteFlexDataSourceItem<UITableViewCell>(identifier: "Test")
+        let type2 = ConcreteFlexDataSourceItem<UITableViewCell>(identifier: "Test2")
+        let type3 = ConcreteFlexDataSourceItem<UITableViewCell>(identifier: "Test3")
+        let array: [FlexDataSourceItem]? = [type, type2, type3]
+        let flexSection = FlexDataSourceSection(title: title, items: array)
+        XCTAssertEqual(flexSection.title, title)
+        XCTAssertTrue(flexSection.items != nil)
+        XCTAssertEqual(flexSection.items?.count, 3)
     }
     
     func testItemsToSection() {
+        let title = "Items to Section"
+        let item1 = ConcreteFlexDataSourceItem<UITableViewCell>(identifier: "1")
+        let item2 = ConcreteFlexDataSourceItem<UITableViewCell>(identifier: "2")
+        let item3 = ConcreteFlexDataSourceItem<UITableViewCell>(identifier: "3")
+        let array: [FlexDataSourceItem] = [item1, item2, item3]
+        itemsToSection(items: array)
+        itemsToSection(items: array).title = title
+        XCTAssertTrue(itemsToSection(items: array).items?.count == 3)
+        XCTAssertTrue(itemsToSection(items: array).items != nil)
+        XCTAssertEqual(itemsToSection(items: array).title, title)
         // make sure that a call to this method accurately creates a FlexDataSourceSection from a list of Items
     }
     
     func testSectionsToDataSource() {
+        let title1 = "First Section"
+        let title2 = "Second Section"
+        let title3 = "Third Section"
+        let type1 = ConcreteFlexDataSourceItem<UITableViewCell>(identifier: "1")
+        let type2 = ConcreteFlexDataSourceItem<UITableViewCell>(identifier: "2")
+        let type3 = ConcreteFlexDataSourceItem<UITableViewCell>(identifier: "3")
+        let type4 = ConcreteFlexDataSourceItem<UITableViewCell>(identifier: "4")
+        let type5 = ConcreteFlexDataSourceItem<UITableViewCell>(identifier: "5")
+        let type6 = ConcreteFlexDataSourceItem<UITableViewCell>(identifier: "6")
+        let type7 = ConcreteFlexDataSourceItem<UITableViewCell>(identifier: "7")
+        let type8 = ConcreteFlexDataSourceItem<UITableViewCell>(identifier: "8")
+        let type9 = ConcreteFlexDataSourceItem<UITableViewCell>(identifier: "9")
+        let array1 = [type1, type2, type3]
+        let array2 = [type4, type5, type6]
+        let array3 = [type7, type8, type9]
+        let flexSection1 = FlexDataSourceSection(title: title1, items: array1)
+        let flexSection2 = FlexDataSourceSection(title: title2, items: array2)
+        let flexSection3 = FlexDataSourceSection(title: title3, items: array3)
+        let sectionArray = [flexSection1, flexSection2, flexSection3]
+        sectionsToDataSource(sections: sectionArray)
+        XCTAssertTrue(sectionsToDataSource(sections: sectionArray).sections?.count != nil)
+        XCTAssertTrue(sectionsToDataSource(sections: sectionArray).sections?.count == 3)
+        XCTAssertTrue(sectionsToDataSource(sections: sectionArray).sections?.first?.title == title1)
+        XCTAssertTrue(sectionsToDataSource(sections: sectionArray).sections?.last?.title == title3)
+        XCTAssertTrue(((sectionsToDataSource(sections: sectionArray).sections?.contains(flexSection2)) != nil))
         // make sure that a call to this method accurately creates a FlexDataSource from a list of sections
     }
 }
